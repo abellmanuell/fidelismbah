4cc504b78d6cc421eb36f82ff4da6468f5d2689a
abellmanuell <mannydev02@gmail.com>
2025-05-09 22:07:06 +0100

    refactor: Replace RouterLink with anchor tag for Learn More link in About component

--- src/app/About.vue
+++ src/app/About.vue
@@ -58,13 +58,13 @@
         />
 
         <div>
-          <RouterLink
-            to="/contact-me"
+          <a
+            href="/contact-me"
             class="text-sm py-2 px-4 rounded-full font-medium inline-block text-primary-500 border transition-all hover:bg-primary-500 hover:text-white"
           >
             <span>LEARN MORE</span>
             <v-icon name="pr-arrow-up-right" scale="1" />
-          </RouterLink>
+          </a>
         </div>
       </div>
     </section>
